ef7f9a88eea7fcfc819b940f3e96792e4a060cd9
EvieePy <evieepy@gmail.com>
2020-10-06 16:09:16 +1000

    Add config to core.

--- core/core.py
+++ core/core.py
@@ -20,8 +20,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__version__ = '0.0.1a'
+import configparser
 
 
-from .bot import Bot
-from .core import *
+__all__ = ('CONFIG', )
+
+
+CONFIG = configparser.RawConfigParser()
+CONFIG.read('config.ini')
