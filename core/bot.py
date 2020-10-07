b5d0f239da7059c709acfd477c4922da07d1b77e
EvieePy <evieepy@gmail.com>
2020-10-08 00:46:52 +1000

    Add intents to Bot.

--- core/bot.py
+++ core/bot.py
@@ -21,13 +21,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 import aiohttp
+import discord
 from discord.ext import commands
 
 
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('>>'))
+        # Default prefix for now for testing, we can change this later...
+        super().__init__(command_prefix=commands.when_mentioned_or('>>'), intents=discord.Intents.all())
 
     async def __ainit__(self) -> None:
         self.session = aiohttp.ClientSession()
