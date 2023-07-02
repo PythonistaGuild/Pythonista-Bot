0c13d2f9fd94f615ef31532d53bdfaef3616579b
EvieePy <evieepy@gmail.com>
2023-07-03 08:14:20 +1000

    Update subscription name as per API.

--- modules/api.py
+++ modules/api.py
@@ -48,13 +48,21 @@ class API(core.Cog):
     def __init__(self, bot: core.Bot) -> None:
         self.bot = bot
 
+        self.session: aiohttp.ClientSession | None = None
         self.backoff: ExponentialBackoff = ExponentialBackoff()  # type: ignore
         self.websocket: aiohttp.ClientWebSocketResponse | None = None
 
         self.connection_task: asyncio.Task[None] | None = None
         self.keep_alive_task: asyncio.Task[None] | None = None
 
+    @property
+    def headers(self) -> dict[str, Any]:
+        return {
+            'Authorization': core.CONFIG['TOKENS']['pythonista']
+        }
+
     async def cog_load(self) -> None:
+        self.session = aiohttp.ClientSession(headers=self.headers)
         self.connection_task = asyncio.create_task(self.connect())
 
     async def cog_unload(self) -> None:
@@ -74,6 +82,10 @@ class API(core.Cog):
             except Exception as e:
                 LOGGER.debug(f'Unable to cancel Pythonista API keep_alive_task in "cog_unload": {e}')
 
+    def dispatch(self, *, data: dict[str, Any]) -> None:
+        subscription: str = data["subscription"]
+        self.bot.dispatch(f"papi_{subscription}", data)
+
     def is_connected(self) -> bool:
         return self.websocket is not None and not self.websocket.closed
 
@@ -90,27 +102,23 @@ class API(core.Cog):
             except Exception as e:
                 LOGGER.debug(f"Failed to cancel Pythonista API Websocket keep alive. This is likely not a problem: {e}")
 
-        headers: dict[str, str] = {"Authorization": token}
-
         while True:
-            async with aiohttp.ClientSession(headers=headers) as session:
-                try:
-                    self.websocket = await session.ws_connect(url=WS_URL)  # type: ignore
-                except Exception as e:
-                    if isinstance(e, aiohttp.WSServerHandshakeError) and e.status == 403:
-                        LOGGER.critical("Unable to connect to Pythonista API Websocket, due to an incorrect token.")
-                        return
-                    else:
-                        LOGGER.debug(f"Unable to connect to Pythonista API Websocket: {e}.")
-
-                if self.is_connected():
-                    session.detach()
-                    break
+            try:
+                self.websocket = await self.session.ws_connect(url=WS_URL)  # type: ignore
+            except Exception as e:
+                if isinstance(e, aiohttp.WSServerHandshakeError) and e.status == 403:
+                    LOGGER.critical("Unable to connect to Pythonista API Websocket, due to an incorrect token.")
+                    return
                 else:
-                    delay: float = self.backoff.delay()  # type: ignore
-                    LOGGER.debug(f'Retrying Pythonista API Websocket connection in "{delay}" seconds.')
+                    LOGGER.debug(f"Unable to connect to Pythonista API Websocket: {e}.")
 
-                    await asyncio.sleep(delay)
+            if self.is_connected():
+                break
+            else:
+                delay: float = self.backoff.delay()  # type: ignore
+                LOGGER.debug(f'Retrying Pythonista API Websocket connection in "{delay}" seconds.')
+
+                await asyncio.sleep(delay)
 
         self.connection_task = None
         self.keep_alive_task = asyncio.create_task(self.keep_alive())
@@ -120,7 +128,7 @@ class API(core.Cog):
 
         initial: dict[str, Any] = {
             "op": PAPIWebsocketOPCodes.SUBSCRIBE,
-            "subscriptions": [PAPIWebsocketSubscriptions.DPY_MOD_LOG],
+            "subscriptions": [PAPIWebsocketSubscriptions.DPY_MODLOG],
         }
         await self.websocket.send_json(data=initial)
 
@@ -133,7 +141,7 @@ class API(core.Cog):
                 aiohttp.WSMsgType.CLOSE,
             )
             if message.type in closing:  # pyright: ignore[reportUnknownMemberType]
-                LOGGER.debug(f"Received a CLOSING/CLOSED/CLOSE message type from Pythonista API.")
+                LOGGER.debug("Received a CLOSING/CLOSED/CLOSE message type from Pythonista API.")
 
                 self.connection_task = asyncio.create_task(self.connect())
                 return
@@ -142,13 +150,10 @@ class API(core.Cog):
             op: int | None = data.get("op")
 
             if op == PAPIWebsocketOPCodes.HELLO:
-                LOGGER.info(f'Received HELLO from Pythonista API: `user={data["user_id"]}`')
+                LOGGER.info(f'Received HELLO from Pythonista API: user={data["user_id"]}')
 
             elif op == PAPIWebsocketOPCodes.EVENT:
-                subscription: str = data["subscription"]
-
-                if subscription == PAPIWebsocketSubscriptions.DPY_MOD_LOG:
-                    self.bot.dispatch("papi_dpy_modlog", data["payload"])
+                self.dispatch(data=data)
 
             elif op == PAPIWebsocketOPCodes.NOTIFICATION:
                 type_: str = data["type"]
